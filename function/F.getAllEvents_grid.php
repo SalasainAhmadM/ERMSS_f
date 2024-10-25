5fa998ba05d5c76a8a5915b23af77b46b61f2fa9
Nadeer Mukaram <nadeermukaram@gmail.com>
2024-10-25 22:26:02 +0800

    total events ui list fix for user/admin

--- function/F.getAllEvents_grid.php
+++ function/F.getAllEvents_grid.php
@@ -2,8 +2,8 @@
     // Include your database connection code here
     require_once('../db.connection/connection.php');
 
-    // Fetch upcoming and ongoing events from the database, excluding canceled events
-    $sql = "SELECT * FROM Events WHERE NOW() < CONCAT(date_end, ' ', time_end) AND (event_cancel IS NULL OR event_cancel = '') ORDER BY date_created DESC";
+    // Fetch all events from the database, excluding canceled events
+    $sql = "SELECT * FROM Events WHERE event_cancel IS NULL OR event_cancel = '' ORDER BY date_created DESC";
     $result = mysqli_query($conn, $sql);
 
     // Loop through each event and generate a box
@@ -24,42 +24,41 @@
         $eventStartDateTime = new DateTime($row['date_start'] . ' ' . $row['time_start'], $eventTimeZone);
         $eventEndDateTime = new DateTime($row['date_end'] . ' ' . $row['time_end'], $eventTimeZone);
 
-        // Check if the event is ongoing or upcoming
+        // Check event status for display purposes
         $eventStatus = '';
-
         if ($currentDateTime >= $eventStartDateTime && $currentDateTime <= $eventEndDateTime) {
             $eventStatus = 'ongoing';
         } elseif ($currentDateTime < $eventStartDateTime) {
             $eventStatus = 'upcoming';
+        } else {
+            $eventStatus = 'ended';
         }
 
-        // Only display upcoming and ongoing events
-        if ($eventStatus === 'upcoming' || $eventStatus === 'ongoing') {
+        // Display all events (upcoming, ongoing, and ended)
 ?>
-            <div class="box" data-start-date="<?php echo $eventStartDateTime->format('Y-m-d H:i:s'); ?>" data-end-date="<?php echo $eventEndDateTime->format('Y-m-d H:i:s'); ?>">
-                <div class="company">
-                    <img src="img/wesmaarrdec-removebg-preview.png" alt="">
-                    <div>
-                        <h3><?php echo $eventTitle; ?></h3>
-                        <span><?php echo $eventMode; ?></span>
-                    </div>
-                </div>
-
-                <h3 class="event-title"><?php echo $eventType; ?></h3>
-
-                <p class="location"><i class="fas fa-map-marker-alt"></i> <span><?php echo $eventLocation; ?></span></p>
-                <div class="tags">
-                    <p><i class='bx bx-calendar' ></i> <span><?php echo "$eventDateStart - $eventDateEnd"; ?></span></p>
-                    <p><i class='bx bxs-timer'></i> <span><?php echo $eventStatus; ?></span></p>
-                    <p><i class="fas fa-clock"></i> <span><?php echo "$eventTimeStart - $eventTimeEnd"; ?></span></p>
-                </div>
-
-                <div class="flex-btn">
-                    <a href="view_eventHistory.php?event_id=<?php echo $row['event_id']; ?>" class="btn">view event</a>
+        <div class="box" data-start-date="<?php echo $eventStartDateTime->format('Y-m-d H:i:s'); ?>" data-end-date="<?php echo $eventEndDateTime->format('Y-m-d H:i:s'); ?>">
+            <div class="company">
+                <img src="img/wesmaarrdec-removebg-preview.png" alt="">
+                <div>
+                    <h3><?php echo $eventTitle; ?></h3>
+                    <span><?php echo $eventMode; ?></span>
                 </div>
             </div>
+
+            <h3 class="event-title"><?php echo $eventType; ?></h3>
+
+            <p class="location"><i class="fas fa-map-marker-alt"></i> <span><?php echo $eventLocation; ?></span></p>
+            <div class="tags">
+                <p><i class='bx bx-calendar'></i> <span><?php echo "$eventDateStart - $eventDateEnd"; ?></span></p>
+                <p><i class='bx bxs-timer'></i> <span><?php echo $eventStatus; ?></span></p>
+                <p><i class="fas fa-clock"></i> <span><?php echo "$eventTimeStart - $eventTimeEnd"; ?></span></p>
+            </div>
+
+            <div class="flex-btn">
+                <a href="view_eventHistory.php?event_id=<?php echo $row['event_id']; ?>" class="btn">view event</a>
+            </div>
+        </div>
 <?php
-        }
     }
     // Close the result set
     mysqli_free_result($result);
