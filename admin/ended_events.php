fbe1f5f5a6ead59100c00219d717d6062939fcbc
SalasainAhmadM <salasainahmad@gmail.com>
2024-11-10 22:17:39 +0800

    all goods filter user side

--- admin/ended_events.php
+++ admin/ended_events.php
@@ -222,12 +222,32 @@ $totalEnded = mysqli_fetch_assoc($totalEndedResult)['totalEnded'];
             </li>
 
             <li class="nav-sidebar">
-                <a href="../login.php">
+                <a href="#" onclick="confirmLogout(event)">
                     <i class="bx bx-log-out"></i>
                     <span class="nav-item">Logout</span>
                 </a>
                 <span class="tooltip">Logout</span>
             </li>
+
+            <script>
+                function confirmLogout(event) {
+                    event.preventDefault();
+
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: "You will be logged out.",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, logout'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = "../login.php";
+                        }
+                    });
+                }
+            </script>
         </ul>
     </div>
 
@@ -479,7 +499,7 @@ $totalEnded = mysqli_fetch_assoc($totalEndedResult)['totalEnded'];
 
 </body>
 
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     function filterEvents(eventType) {
         // Get all rows in the events table
